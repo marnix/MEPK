8819377146a058207c8903810f0341d0339d0618
Marnix Klooster <marnix.klooster@gmail.com>
2014-03-07 22:39:48 +0100

    A few JavaDoc updates, mainly describing package mepk.builtin.

--- src/main/java/mepk/builtin/package-info.java
+++ src/main/java/mepk/builtin/package-info.java
@@ -1,4 +1,9 @@
 /**
  * This package implements extensions on top of the {@link mepk.kernel}.
+ * The main entry point is {@link mepk.builtin.TrustedProof}.
+ * The code was carefully designed to make sure that {@link mepk.kernel.Proof#verify()}
+ * always succeeds for a {@code TrustedProof};
+ * but in case of doubt you can always {@code verify()} that. 
+
  */
 package mepk.builtin;
--- src/main/java/mepk/package-info.java
+++ src/main/java/mepk/package-info.java
@@ -12,7 +12,7 @@
  * each proof can be verified using {@link mepk.kernel.Proof#verify()}.
  * <p> 
  * The {@link mepk.builtin} package contains some basic proofs and proof combinators.
- * The entry point is {@link mepk.builtin.TrustedProof}.
+ * The main entry point is {@link mepk.builtin.TrustedProof}.
  * The code was carefully designed to make sure that {@link mepk.kernel.Proof#verify()}
  * always succeeds for a {@code TrustedProof};
  * but in case of doubt you can always {@code verify()} that. 
