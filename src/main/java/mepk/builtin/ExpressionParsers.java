6cc94e80754d87908aa1fc19247585a595a5bb23
Marnix Klooster <marnix.klooster@gmail.com>
2014-03-10 19:32:57 +0100

    Rewritten the JParsec parsers based on tokens.
    This style makes it easier to add new Statement and Theory parsers
    later.  This commits adds a restriction that only 'identifiers'
    and single-quoted string literals are allowed, so, e.g., (0) is
    not yet allowed.

--- src/main/java/mepk/builtin/ExpressionParsers.java
+++ src/main/java/mepk/builtin/ExpressionParsers.java
@@ -1,15 +1,16 @@
 package mepk.builtin;
 
 import static mepk.kernel.Expression.*;
-import static org.codehaus.jparsec.Parsers.*;
-import static org.codehaus.jparsec.Scanners.*;
 
 import java.util.List;
 
 import mepk.kernel.Expression;
 
 import org.codehaus.jparsec.Parser;
+import org.codehaus.jparsec.Parsers;
 import org.codehaus.jparsec.Scanners;
+import org.codehaus.jparsec.Terminals;
+import org.codehaus.jparsec.Token;
 import org.codehaus.jparsec.functors.Map;
 import org.codehaus.jparsec.functors.Pair;
 
@@ -20,61 +21,56 @@ import org.codehaus.jparsec.functors.Pair;
  */
 public class ExpressionParsers {
 
+	private static final Terminals OPERATORS = Terminals.operators("(", ")");
+
+	private static Parser<Token> term(String tokenName) {
+		return OPERATORS.token(tokenName);
+	}
+
+	public static final Parser<Expression> VARIABLE;
+
+	public static final Parser<Expression> APPLICATION;
+
+	private static final Parser<Expression> EXPRESSION_;
+
+	public static final Parser<Expression> EXPRESSION;
+
+	static {
+		Parser<?> ignored = Parsers.or(Scanners.lineComment(";"), Scanners.WHITESPACES);
+
+		Parser<?> idTokenizer = Parsers.or(Terminals.Identifier.TOKENIZER, Terminals.StringLiteral.SINGLE_QUOTE_TOKENIZER);
+		Parser<?> tokenizer = Parsers.or(OPERATORS.tokenizer(), idTokenizer);
+
+		Parser.Reference<Expression> exprRef = new Parser.Reference<>();
+		Parser<String> identifier_ = Terminals.Identifier.PARSER.or(Terminals.StringLiteral.PARSER);
+		Parser<Expression> application_ = Parsers.pair(identifier_, exprRef.lazy().many()).between(term("("), term(")"))
+				.map(new Map<Pair<String, List<Expression>>, Expression>() {
+					@Override
+					public Expression map(Pair<String, List<Expression>> from) {
+						return App(from.a, from.b.toArray(new Expression[from.b.size()]));
+					}
+				});
+
+		Parser<Expression> variable_ = identifier_.map(
+				new Map<String, Expression>() {
+					@Override
+					public Expression map(String from) {
+						return Var(from);
+					}
+				});
+		EXPRESSION_ = application_.or(variable_);
+		exprRef.set(EXPRESSION_);
+
+		VARIABLE = variable_.from(tokenizer, ignored.skipMany());
+		APPLICATION = application_.from(tokenizer, ignored.skipMany());
+		EXPRESSION = EXPRESSION_.from(tokenizer, ignored.skipMany());
+	}
+
 	/**
 	 * This constructor should not be used: this class contains only static
 	 * methods.
 	 */
 	private ExpressionParsers() {
-	}
-
-	/**
-	 * A JParsec parser for an expression consisting of just a variable.
-	 * 
-	 * @return the parser
-	 */
-	public static Parser<Expression> variable() {
-		return IDENTIFIER.map(new Map<String, Expression>() {
-			@Override
-			public Expression map(String name) {
-				return Var(name);
-			}
-		});
-	}
-
-	/**
-	 * A JParsec parser for an application expression.
-	 * 
-	 * @return the parser
-	 */
-	public static Parser<Expression> application() {
-		final Parser.Reference<Expression> expressionRef = new Parser.Reference<Expression>();
-		Parser<List<Expression>> exprList = expressionRef.lazy().sepBy1(WHITESPACES);
-		Parser<Pair<String, List<Expression>>> applicationRaw = tuple(token(), WHITESPACES.next(exprList).optional())
-				.between(isChar('('), isChar(')'));
-		final Parser<Expression> application = applicationRaw.map(new Map<Pair<String, List<Expression>>, Expression>() {
-			@Override
-			public Expression map(Pair<String, List<Expression>> tuple) {
-				return tuple.b == null ? App(tuple.a) : App(tuple.a, tuple.b.toArray(new Expression[tuple.b.size()]));
-			}
-		});
-		// NOTE the duplication with method expression(), which seems
-		// unavoidable because of the mutual recursion.
-		expressionRef.set(variable().or(application));
-		return application;
-	}
-
-	/**
-	 * A JParsec parser for an expression, i.e., a variable or an application of
-	 * a constant to a list of zero or more expressions.
-	 * 
-	 * @return the parser
-	 */
-	public static Parser<Expression> expression() {
-		return variable().or(application());
-	}
-
-	private static Parser<String> token() {
-		return Scanners.notAmong("() \t\n\r").many1().source();
 	}
 
 }
--- src/test/java/mepk/test/TestParser.java
+++ src/test/java/mepk/test/TestParser.java
@@ -11,19 +11,19 @@ public class TestParser {
 
 	@Test
 	public void testParseIdentifierAsVariable() throws Throwable {
-		assertEquals(Var("x"), variable().parse("x"));
-		assertEquals(Var("x"), expression().parse("x"));
+		assertEquals(Var("x"), VARIABLE.parse("x"));
+		assertEquals(Var("x"), EXPRESSION.parse("x"));
 	}
 
 	@Test
 	public void testFailParseNonIdentifier() throws Throwable {
 		try {
-			variable().parse("0");
+			VARIABLE.parse("0");
 			fail();
 		} catch (ParserException e) {
 		}
 		try {
-			expression().parse("0");
+			EXPRESSION.parse("0");
 			fail();
 		} catch (ParserException e) {
 		}
@@ -31,9 +31,11 @@ public class TestParser {
 
 	@Test
 	public void testParseListExpressionAsApplication() throws Throwable {
-		assertEquals(App("0"), application().parse("(0)"));
-		assertEquals(App("0"), expression().parse("(0)"));
-		assertEquals(App("-", Var("x")), application().parse("(- x)"));
-		assertEquals(App("-", Var("x")), expression().parse("(- x)"));
+		// TODO: Make (0) legal, instead of having to use ('0')
+		assertEquals(App("0"), APPLICATION.parse("('0')"));
+		assertEquals(App("0"), EXPRESSION.parse("('0')"));
+		assertEquals(App("-", Var("x")), APPLICATION.parse("('-' x)"));
+		assertEquals(App("-", Var("x")), EXPRESSION.parse("('-' x)"));
+		assertEquals(App("0", Var("b2"), Var("c3"), Var(";'")), EXPRESSION.parse("('0' b2 ; this is comment\nc3 ';''')"));
 	}
 }
